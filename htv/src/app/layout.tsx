ae4cbe24296f63e1f652964962f798aed2f7d36d
txxshalott <txxshalott@gmail.com>
2024-10-05 18:03:15 -0400

    home page layout done

--- htv/src/app/layout.tsx
+++ htv/src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "../styles/globals.css";
 import React from "react";
-import MainPageBar from "./mainNavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
